a03175ca9e158aaa0ec6bd760e64291955b194ba
sam-klok <sklok11@gmail.com>
2021-08-08 18:58:15 -0700

    corrected some queries

--- Child Father Mother - correct.sql
+++ Child Father Mother - correct.sql
@@ -1,20 +1,28 @@
 -- return table Child - Father - Mother
 -- this query return properly 
 
+-- c - child
+-- f - father
+-- m - mother
+
 select 
-	--  r1.c_id
-	--, r1.p_id
-	--, r2.p_id
 	  c.name 'Child'
 	, f.name 'Father'
 	, m.name 'Mother'
-
-
 from relations r1
-left join relations r2 on r1.c_id = r2.c_id and r1.p_id <> r2.p_id
-join people c on  r1.c_id = c.id
-join people f on  r1.p_id = f.id and f.gender = 'M'
-join people m on  r2.p_id = m.id and m.gender = 'F'
+left join relations r2 on  r1.c_id = r2.c_id and r1.p_id <> r2.p_id
+join people c on  r1.c_id = c.id  -- child
+	join people f on  r1.p_id = f.id and f.gender = 'M'
+	left join people m on  r2.p_id = m.id and m.gender = 'F'
 
+union
 
--- John	Sam	Jessica
+select 
+	  c.name 'Child'
+	, f.name 'Father'
+	, m.name 'Mother'
+from relations r1
+left join relations r2 on  r1.c_id = r2.c_id and r1.p_id <> r2.p_id
+join people c on  r1.c_id = c.id -- child
+	join people m on  r1.p_id = m.id and m.gender = 'F'
+	left join people f on  r2.p_id = f.id and f.gender = 'M'
--- create tables people relations.sql
+++ create tables people relations.sql
@@ -15,14 +15,25 @@ CREATE TABLE [dbo].[relations](
 GO
 
 
-INSERT [dbo].[people] ([id], [name], [gender]) VALUES (1, N'John', N'M')
-GO
-INSERT [dbo].[people] ([id], [name], [gender]) VALUES (2, N'Sam', N'M')
-GO
-INSERT [dbo].[people] ([id], [name], [gender]) VALUES (3, N'Jessica', N'F')
-GO
+INSERT [dbo].[people] ([id], [name], [gender]) VALUES (1, 'John', 'M')
+INSERT [dbo].[people] ([id], [name], [gender]) VALUES (2, 'Sam', 'M')
+INSERT [dbo].[people] ([id], [name], [gender]) VALUES (3, 'Jessica', 'F')
+
+-- child of single mom
+insert people values (4, 'Slava','M')
+insert people values (5, 'Katia','F')
+
+-- child of single dad
+insert people values (6, 'Anastasiya','F')
+insert people values (7, 'Yuri','M')
 
 INSERT [dbo].[relations] ([c_id], [p_id]) VALUES (1, 2)
-GO
 INSERT [dbo].[relations] ([c_id], [p_id]) VALUES (1, 3)
-GO
+
+insert relations values (4, 5)
+insert relations values (6, 7)
+
+
+-- validate
+select * from people
+select * from relations
