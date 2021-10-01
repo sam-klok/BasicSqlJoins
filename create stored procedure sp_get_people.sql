a350a600eb63f6ba13f3f1279ef18ba56b6e22d2
sam-klok <sklok11@gmail.com>
2021-09-30 18:34:53 -0700

    modify creation script

--- create stored procedure sp_get_people.sql
+++ create stored procedure sp_get_people.sql
@@ -2,10 +2,11 @@ DROP PROCEDURE IF EXISTS sp_get_people
 go
 
 
-CREATE PROCEDURE sp_get_people
+CREATE PROCEDURE [dbo].[sp_get_people]
 AS
+-- stored procedure created by Sam Klok
+-- to run
+-- exec dbo.sp_get_people
 SELECT * FROM people
 go
 
-exec dbo.sp_get_people
-go
--- create view v_people.sql
+++ create view v_people.sql
@@ -1,8 +1,13 @@
 use LifelongLearning
 go
 
-create view v_people 
+DROP view IF EXISTS [dbo].[v_people] 
+go
+
+
+create view [dbo].[v_people]
 as 
+-- new view for demo
 select id,name from dbo.people
 go
 
